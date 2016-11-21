ee0e1aadce5bc07f17a21cec64f2801b372c214a
ashutoshpurushottam <apurushottam@icloud.com>
2016-11-21 22:34:12 +0530

    functioning code for now

--- static/js/init.js
+++ static/js/init.js
@@ -9,26 +9,6 @@ $(window).scroll(function() {
 	}
 });
 
-// image preview in create genre
-// $('#poster').change(function() {
-// 	console.log("poster chagned");
-// 	var posterUrl = $('#poster').val();
-// 	console.log(posterUrl);
-// 	var posterName = posterUrl.split('\\')[2];
-// 	$('#image-div').show();
-// 	var image = $('#preview-image');
-// 	var source = 'static/image/' + posterName;
-// 	console.log(source);
-// 	image.attr('src', source);
-// });
-// $('#poster').on('click', function() {
-// 	console.log("inside image");
-// 	var posterUrl = $('#poster').val();
-// 	$('#image-div').show();
-// 	var image = $('#preview-image');
-// 	image.attr('src', 'static/image/' + posterUrl);
-// });
-
 // Show image on focus in edit genre
 $('#poster_url').focus(function() {
 	var posterUrl = $('#poster_url').val();
